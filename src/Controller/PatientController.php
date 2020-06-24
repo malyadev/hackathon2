2d549321ce1b304fdb42b0658e8f985b5d3ae725
malyadev <malyadev248@gmail.com>
2020-06-24 23:54:19 +0200

    show prescription ok

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Prescription;
+use App\Entity\PrescriptionDrug;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("/patient", name="patient")
@@ -15,8 +19,27 @@ class PatientController extends AbstractController
     /**
      * @Route("/prescription/index", name="_prescription_index")
      */
-    public function index()
+    public function index(? UserInterface $user)
     {
-        return $this->render(self::ROLE.'/index.html.twig');
+        $prescription = $this->getDoctrine()
+            ->getRepository(Prescription::class)
+            ->findOneBy(
+                ['user' => $user]
+            );
+
+        if (!is_null($prescription)) {
+            $prescriptionDrugs = $this->getDoctrine()
+                ->getRepository(PrescriptionDrug::class)
+                ->findBy(
+                    ['prescription' => $prescription]
+                );
+
+            if ($prescriptionDrugs) {
+                return $this->render(self::ROLE . '/index.html.twig', [
+                    'prescriptionDrugs' => $prescriptionDrugs,
+                    'user' => $user
+                ]);
+            }
+        }
     }
 }
