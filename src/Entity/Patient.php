6aa8430a2b472f31cdb9a9df7fa1a75e0e15327a
mayard689 <doc-albert@laposte.net>
2020-06-26 08:38:51 +0200

    add prefered pharmacy and alternative propositions

---
+++ src/Entity/Patient.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PatientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PatientRepository::class)
+ */
+class Patient
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity=User::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $patient;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Pharmacy::class)
+     */
+    private $pharmacy;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPatient(): ?User
+    {
+        return $this->patient;
+    }
+
+    public function setPatient(User $patient): self
+    {
+        $this->patient = $patient;
+
+        return $this;
+    }
+
+    public function getPharmacy(): ?Pharmacy
+    {
+        return $this->pharmacy;
+    }
+
+    public function setPharmacy(?Pharmacy $pharmacy): self
+    {
+        $this->pharmacy = $pharmacy;
+
+        return $this;
+    }
+}
